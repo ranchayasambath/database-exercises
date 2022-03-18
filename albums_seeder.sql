338272c58f36a24f9d1603c024b80340192ff716
ranchayasambath <ranchayasambath@gmail.com>
2022-03-18 11:47:44 -0500

    updated albums_seeder

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,63 +1,63 @@
 USE codeup_test_db;
 TRUNCATE albums;
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop', 49.2);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'AC/DC', 'Back in Black', 1980, 'Hard Rock', 29.5);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Whitney Houston', 'The Bodyguard', 1992, 'Pop', 32.4);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Meat Loaf', 'Bat Out of Hell', 1992, 'Hard Rock', 21.7);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Eagles', 'Hotel California', 1976, 'Country Rock', 41.2);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Bee Gees', 'Saturday Night Fever', 1977, 'Rock', 21.6);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Fleetwood Mac', 'Rumors', 1977, 'Soft Rock', 27.9);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Shania Twain', 'Come On Over', 1997, 'Disco', 29.6);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Led Zeppelin', 'Led Zepplin IV', 1971, 'Soft Rock', 29);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Michael Jackson', 'Bad', 1987, 'Pop', 21.1);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Alanis Morissette', 'Jagged Little Pill', 1995, 'Rock', 24.3);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Michael Jackson', 'Dangerous', 1991, 'Hard Rock', 17);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Celine Dion', 'Falling into you', 1996, 'Pop', 21.1);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 'Rock', 18.2);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Adele', '21', 2011, 'Pop', 26.4);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Celine Dion', 'Let''s Talk About Love', 1997, 'Pop', 19.3);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Metallica', 'Metallica', 1991, 'Heavy Metal', 25.2);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'The Beatles', '1', 2000, 'Rock', 23.5);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'The Beatles', 'Abbey Road', 1969, 'Rock', 14.4);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'ABBA', 'Gold', 1992, 'Pop', 23.0);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Bruce Springsteen', 'Born in the U.S.A', 1984, 'Heartland Rock', 19.6);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Madonna', 'The Immaculate Collection', 1990, 'Pop', 19.5);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Pink Floyd', 'The Wall', 1979, 'Progressive Rock', 18.7);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'James Horner', 'Titanic', 1997, 'Film Score', 18.1);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Dire Straits', 'Brothers in Arms', 1985, 'Roots Rock', 17.7);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Nirvana', 'Nevermind', 1991, 'Grunge', 16.7);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Santana', 'Supernatural', 1999, 'Latin Rock', 20.5);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Guns N'' Roses', 'Appetite for Destruction', 1987, 'Hard Rock', 21.9);
-INSERT INTO albums(artist, name, release_date, genre, sales)
-VALUES ( 'Elton John', 'Goodbye Yellow Brick Road', 1973, 'Rock', 8.5);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop', 49.2);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'AC/DC', 'Back in Black', 1980, 'Hard Rock', 29.5);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Whitney Houston', 'The Bodyguard', 1992, 'Pop', 32.4);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Meat Loaf', 'Bat Out of Hell', 1992, 'Hard Rock', 21.7);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Eagles', 'Hotel California', 1976, 'Country Rock', 41.2);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Bee Gees', 'Saturday Night Fever', 1977, 'Rock', 21.6);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Fleetwood Mac', 'Rumors', 1977, 'Soft Rock', 27.9);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Shania Twain', 'Come On Over', 1997, 'Disco', 29.6);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Led Zeppelin', 'Led Zepplin IV', 1971, 'Soft Rock', 29);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Michael Jackson', 'Bad', 1987, 'Pop', 21.1);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Alanis Morissette', 'Jagged Little Pill', 1995, 'Rock', 24.3);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Michael Jackson', 'Dangerous', 1991, 'Hard Rock', 17);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Celine Dion', 'Falling into you', 1996, 'Pop', 21.1);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 'Rock', 18.2);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Adele', '21', 2011, 'Pop', 26.4);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Celine Dion', 'Let''s Talk About Love', 1997, 'Pop', 19.3);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Metallica', 'Metallica', 1991, 'Heavy Metal', 25.2);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'The Beatles', '1', 2000, 'Rock', 23.5);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'The Beatles', 'Abbey Road', 1969, 'Rock', 14.4);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'ABBA', 'Gold', 1992, 'Pop', 23.0);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Bruce Springsteen', 'Born in the U.S.A', 1984, 'Heartland Rock', 19.6);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Madonna', 'The Immaculate Collection', 1990, 'Pop', 19.5);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Pink Floyd', 'The Wall', 1979, 'Progressive Rock', 18.7);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'James Horner', 'Titanic', 1997, 'Film Score', 18.1);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Dire Straits', 'Brothers in Arms', 1985, 'Roots Rock', 17.7);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Nirvana', 'Nevermind', 1991, 'Grunge', 16.7);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Santana', 'Supernatural', 1999, 'Latin Rock', 20.5);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Guns N'' Roses', 'Appetite for Destruction', 1987, 'Hard Rock', 21.9);
+-- INSERT INTO albums(artist, name, release_date, genre, sales)
+-- VALUES ( 'Elton John', 'Goodbye Yellow Brick Road', 1973, 'Rock', 8.5);
 
 INSERT INTO albums(artist,name,release_date,genre,sales)
 VALUES( 'Michael Jackson', 'Thriller', 1982, 'Pop', 49.2),
