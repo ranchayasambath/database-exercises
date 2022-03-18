9303f05f234ff1c98d1f990f4a284e72c123c5e4
ranchayasambath <ranchayasambath@gmail.com>
2022-03-18 11:21:15 -0500

    completed exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -4,22 +4,24 @@ USE codeup_test_db;
 /*All albums in your table.*/
 /*After each SELECT add an UPDATE statement to:*/
 /*Add SELECT statements after each UPDATE so you can see the results of your handiwork.*/
-SELECT * FROM albums;
-UPDATE albums;
+
+
 /*Make all the albums 10 times more popular (sales * 10)*/
+UPDATE albums
 SET sales = (sales * 10);
 SELECT * FROM albums;
-/*All albums released before 1980*/
-SELECT * FROM albums WHERE release_date < 1980;
-UPDATE albums;
+-- /*All albums released before 1980*/
+-- SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums
 /*Move all the albums before 1980 back to the 1800s.*/
 SET release_date = (release_date - 100) WHERE release_date < 1980;
 /*select from all albums in 1800s*/
-SELECT * FROM albums WHERE release_date < 1900;
-/*All albums by Michael Jackson*/
+SELECT * FROM albums WHERE release_date < 1980;
+-- /*All albums by Michael Jackson*/
 SELECT * FROM albums WHERE artist = 'Michael Jackson';
-UPDATE albums;
 /*Change 'Michael Jackson' to 'Peter Jackson'*/
+UPDATE albums
 SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
-/*select newly updated*/
+-- /*select newly updated*/
 SELECT * FROM albums WHERE artist = 'Peter Jackson';
+
